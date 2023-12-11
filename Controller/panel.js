df6b8d4d2dd818a95c74aefc70566ab8b0b592b5
Josee-cmd <josesslcedo@gmail.com>
2023-12-11 18:15:14 -0500

    Changes in Front Personal Area

--- Controller/panel.js
+++ Controller/panel.js
@@ -3,9 +3,9 @@ date = () => {
     const days = ["lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
     const Months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
     let day = date.getDay();
+    let dayy = date.getDate();
     let Month = date.getMonth();
     const p = document.querySelector('.p');
-    p.append(`${days[day - 1]} ${Month} de ${Months[Month - 1]}`);
+    p.append(`${days[day - 1]} ${dayy} de ${Months[Month]}`);
 }
-
 date();
