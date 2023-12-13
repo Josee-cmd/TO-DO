ca653408ee7e4e1a7027ad42e0fb5050c1d37866
Josee-cmd <josesslcedo@gmail.com>
2023-12-12 21:08:11 -0500

    Front pages personal area completed

--- Controller/panel.js
+++ Controller/panel.js
@@ -1,3 +1,4 @@
+
 date = () => {
     let date = new Date();
     const days = ["lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
@@ -8,4 +9,83 @@ date = () => {
     const p = document.querySelector('.p');
     p.append(`${days[day - 1]} ${dayy} de ${Months[Month]}`);
 }
+addTask = () => {
+    document.addEventListener("DOMContentLoaded", () => {
+        const input = document.querySelector('#input');
+        const ul = document.querySelector('#miLista');
+        const div = document.querySelector('.div');
+        let cont = 0;
+        input.addEventListener('keydown', (event) => {
+            if (event.key === 'Enter') {
+                if (input.value) {
+                    if (cont <= 9) {
+                        div.classList.add('show')
+                        let li = document.createElement('li');
+                        const img = document.createElement('img');
+                        const h6 = document.createElement('h6');
+                        const radio = document.createElement('div');
+                        radio.classList.add('radio');
+                        h6.textContent = "Tarea";
+                        h6.classList.add('h6');
+                        img.src = '../Icons/mas.png';
+                        img.classList.add('img');
+                        const fragmento = document.createDocumentFragment();
+                        fragmento.appendChild(document.createTextNode(input.value));
+                        fragmento.appendChild(document.createTextNode(' '));
+                        fragmento.appendChild(img);
+                        fragmento.appendChild(h6);
+                        fragmento.appendChild(radio);
+                        li.innerHTML = '';
+                        li.appendChild(fragmento);
+                        li.classList.add(`li${cont}`);
+                        ul.appendChild(li);
+                        change(radio);
+                        clean(input);
+                        cont += 1;
+
+                    } else {
+                        alert("Termina las tareas pendientes");
+                        clean(input);
+                    }
+                } else {
+                    alert("Agregue una terea")
+                }
+            }
+
+        });
+    })
+}
+
+let getData =()=>{
+    let data = localStorage.getItem('USER');
+    let dataSave = JSON.parse(data)
+    console.log(dataSave.name);
+
+    console.log(dataSave.email);
+
+    let h4 = document.createElement('h4');
+    let h6 = document.createElement('h6');
+
+    h4.textContent = dataSave.name;
+    h6.textContent = dataSave.email;
+     
+    let fragmento = document.createDocumentFragment();
+    fragmento.appendChild(h4)
+    fragmento.appendChild(h6);
+    let header = document.querySelector('.data');
+    header.appendChild(fragmento)
+
+
+}
+let clean = (input) => {
+    input.value = '';
+}
+
+let change = (radio) => {
+    radio.addEventListener('click', () => {
+        radio.classList.toggle('changeColor');
+    });
+}
+getData();
 date();
+addTask();
